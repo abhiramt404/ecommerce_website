00eb9ecdaeba068a78770b8221939b7c896f091c
abhiramt <abhiramt2018@gmail.com>
2026-04-24 23:08:49 +0530

    updated css , created routers components and routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home';
+import Auth from './pages/auth';
+import Checkout from './pages/checkut';
 
 function App() {
-
-
-  return 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
